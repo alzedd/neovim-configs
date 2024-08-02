1f86de29017092d7a389a2ac11d32a47168c43ba
Alzedd <alzedd@gmail.com>
2024-08-02 15:04:21 +0200

    fix: gitsigns post upgrade

--- lua/user/plugins/gitsigns.lua
+++ lua/user/plugins/gitsigns.lua
@@ -5,11 +5,11 @@ end
 
 gitsigns.setup {
   signs = {
-    add = { hl = "GitSignsAdd", text = "▪", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▪", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "▪", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "▪", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▪", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    add = { text = "▪"},
+    change = { text = "▪"},
+    delete = { text = "▪"},
+    topdelete = { text = "▪"},
+    changedelete = { text = "▪"},
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -20,16 +20,16 @@ gitsigns.setup {
     follow_files = true,
   },
   attach_to_untracked = true,
-  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-  current_line_blame_opts = {
-    virt_text = true,
-    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
-    ignore_whitespace = false,
-  },
-  current_line_blame_formatter_opts = {
-    relative_time = true,
-  },
+  -- current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  -- current_line_blame_opts = {
+  --   virt_text = true,
+  --   virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+  --   delay = 1000,
+  --   ignore_whitespace = false,
+  -- },
+  -- current_line_blame_formatter_opts = {
+  --   relative_time = true,
+  -- },
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
@@ -41,9 +41,6 @@ gitsigns.setup {
     relative = "cursor",
     row = 0,
     col = 1,
-  },
-  yadm = {
-    enable = false,
-  },
+  }
 }
 
