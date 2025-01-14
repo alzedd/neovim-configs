c14b90545d3b050c40717a0a30a352841952f42a
Alzedd <alzedd@gmail.com>
2025-01-14 14:54:57 +0100

    feat: add orgmode

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -51,6 +51,29 @@ require("lazy").setup({
 	-- TERMINAL
 	"akinsho/toggleterm.nvim",
 
+
+
+	-- ORGMODE
+	{
+		'nvim-orgmode/orgmode',
+		event = 'VeryLazy',
+		ft = { 'org' },
+		config = function()
+			-- Setup orgmode
+			require('orgmode').setup({
+				org_agenda_files = '~/orgfiles/**/*',
+				org_default_notes_file = '~/orgfiles/refile.org',
+			})
+
+			-- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+			-- add ~org~ to ignore_install
+			-- require('nvim-treesitter.configs').setup({
+			--   ensure_installed = 'all',
+			--   ignore_install = { 'org' },
+			-- })
+		end,
+	},
+
 	-- TREESITTER
 	"nvim-treesitter/nvim-treesitter",
 
